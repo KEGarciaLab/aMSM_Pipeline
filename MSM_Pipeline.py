d3b8eba622b9222c6896a50f1a7b3a74b9aae960
Sammy Rigdon <sammyrigdon496@gmail.com>
2026-05-11 11:21:37 -0500

    fixed right anatomical surface search

--- MSM_Pipeline.py
+++ MSM_Pipeline.py
@@ -2076,7 +2076,7 @@ def get_files_mcribs(dataset: str, subject: str, time_point: str, is_rescaled=Fa
     print()
     
     print(f'{datetime.now()}[FUNCTION] find(patterns="rh.midthickness.surf.gii", search_path=subject_dir)')
-    right_anatomical_surface = find(patterns="rh.midthickness.surf.gii", search_path=subject_dir)
+    right_anatomical_surface = find(patterns=[f"rh.{subject}{time_point}_midthickness_711-2N_rot.surf.gii", f"rh.{subject}{time_point}_midthickness_711-2N.surf.gii"], search_path=subject_dir)
     print()
     
     print(f'{datetime.now()}[FUNCTION] find(patterns="lh.sphere.reg2.surf.gii", search_path=subject_dir)')
