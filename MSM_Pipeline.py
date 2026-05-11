d1a8e76cd78932944f83db06cd4ad1a14a6127e9
Sammy Rigdon <sammyrigdon496@gmail.com>
2026-05-11 09:10:43 -0500

    QC function uses same surface for both images Fixes #67

--- MSM_Pipeline.py
+++ MSM_Pipeline.py
@@ -502,10 +502,10 @@ def generate_qc_image(dataset: str, subject: str, younger_timepoint: str, older_
         older_files = get_files(dataset, subject, older_timepoint)
         print()
 
-    left_younger_surface = younger_files["LAS"]
-    right_younger_surface = younger_files["RAS"]
-    left_older_surface = older_files["LAS"]
-    right_older_surface = older_files["RAS"]
+    left_younger_surface = younger_files["LEFT RESCALE ANAT"]
+    right_younger_surface = younger_files["LEFT RESCALE ANAT"]
+    left_older_surface = older_files["LEFT RESCALE ANAT"]
+    right_older_surface = older_files["LEFT RESCALE ANAT"]
 
     print("[FILES] Selected surfaces:")
     print(f"    Younger L: {left_younger_surface}")
