3212b5b639256c24fa1076b6bceb0414d725cfad
Sammy Rigdon <sammyrigdon496@gmail.com>
2026-05-11 11:55:19 -0500

    added 3rd fall back on AS

--- MSM_Pipeline.py
+++ MSM_Pipeline.py
@@ -2072,11 +2072,11 @@ def get_files_mcribs(dataset: str, subject: str, time_point: str, is_rescaled=Fa
     # --------------------------
     
     print(f'{datetime.now()}[FUNCTION] find(patterns="lh.midthickness.surf.gii", search_path=subject_dir)')
-    left_anatomical_surface = find(patterns=[f"lh.{subject}{time_point}_midthickness_711-2N_rot.surf.gii", f"lh.{subject}{time_point}_midthickness_711-2N.surf.gii"], search_path=subject_dir)
+    left_anatomical_surface = find(patterns=[f"lh.{subject}{time_point}_midthickness_711-2N_rot.surf.gii", f"lh.{subject}{time_point}_midthickness_711-2N.surf.gii", "lh.midthickness.surf.gii"], search_path=subject_dir)
     print()
     
     print(f'{datetime.now()}[FUNCTION] find(patterns="rh.midthickness.surf.gii", search_path=subject_dir)')
-    right_anatomical_surface = find(patterns=[f"rh.{subject}{time_point}_midthickness_711-2N_rot.surf.gii", f"rh.{subject}{time_point}_midthickness_711-2N.surf.gii"], search_path=subject_dir)
+    right_anatomical_surface = find(patterns=[f"rh.{subject}{time_point}_midthickness_711-2N_rot.surf.gii", f"rh.{subject}{time_point}_midthickness_711-2N.surf.gii", "rh.midthickness.surf.gii"], search_path=subject_dir)
     print()
     
     print(f'{datetime.now()}[FUNCTION] find(patterns="lh.sphere.reg2.surf.gii", search_path=subject_dir)')
